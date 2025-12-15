7b1b341536c9e76b8e8417f18d7fc90650035d01
Martin Ferrero-Thompson <martin.ferrero.thompson@gmail.com>
2025-12-15 10:44:16 +0100

    chore: Update dependencies and regenerate static site build output.

--- .vercel/output/static/_astro/LayoutWrapper.q797EIvF.js
+++ .vercel/output/static/_astro/LayoutWrapper.q797EIvF.js
@@ -1,4 +1,4 @@
-import{j as s}from"./jsx-runtime.D_zvdyIk.js";import{r as _}from"./index.D55ewHcJ.js";/**
+import{j as s}from"./jsx-runtime.D_zvdyIk.js";import{r as _}from"./index.WFquGv8Z.js";/**
  * @license lucide-react v0.554.0 - ISC
  *
  * This source code is licensed under the ISC license.
